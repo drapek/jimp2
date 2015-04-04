bfc1a414ec1831fd021cbafea193e94e94dee127
drapek <drapek39@wp.pl>
2015-04-04 20:29:02 +0200

    fixed and working ngram functions like adding, and initializing

--- src/ngramstruct.c
+++ src/ngramstruct.c
@@ -1,6 +1,8 @@
 #include "ngramstruct.h"
 #include "errors.h"
 
+#define NGRAM_STRUCT_LIMIT 40
+
 /*[IMPORTANT] string_stock functions aren't working! */
 
 /* this functions describes string_stack which store dynamic arrays of string. */
@@ -61,11 +63,40 @@ int string_stock_free(string_stock * what) {
 /*create and initiaze new ngram and return a pointer to them*/
 ngram * ngram_init() {
     ngram * new = malloc( sizeof(* new));
+    new->one_file = malloc( sizeof( new->one_file ) * 10);
+    if( new->one_file == NULL )
+	   program_error(ERR_CRITIC, ERR_NGRAM_STRUCT, "brak pamięci operacyjnej, by stworzyć strukturę przechowującą ngramy (ngram_init)");
+
     new->ngram_size = 10;
     new->ngram_elem = 0;
-    new->one_file = malloc( sizeof( new->one_file ) * 10);
 
     return new;
+}
+
+/*add at end of stucture */
+int ngram_add(ngram * this, char ** words_collect, int num_words, char * file_name) {
+    /*check if there is space to add*/
+    if( (this->ngram_size) <= (this->ngram_elem) ) {
+	   /*resize double*/
+	   /*limit readed ngrams because to few can cause realloc errors */
+	   if( this->ngram_size >= NGRAM_STRUCT_LIMIT ) {
+		  program_error(ERR_CRITIC, ERR_NGRAM_STRUCT, "Zbyt duża liczba wczytanych plików, maksymalnie jest to NGRAM_STRUCT_LIMIT"); 
+		  return 0;
+		  }
+
+	   void * new =  realloc( this->one_file, sizeof( *new ) * this->ngram_size * 2);
+	   if( new == NULL )
+		  program_error(ERR_CRITIC, ERR_NGRAM_STRUCT, "brak pamięci operacyjnej, by powiększyć strukturę przechowującą ngramy (ngram_add function error)");
+	   else {
+		  this->one_file = new;
+		  this->ngram_size *= 2;
+	   }
+    }
+
+    this->one_file[this->ngram_elem].num_words = num_words;
+    this->one_file[this->ngram_elem].name_file = file_name;
+    this->one_file[this->ngram_elem].words = words_collect;
+    this->ngram_elem++;
 }
 
 /* [ABONDED] search in ngram word collection which are the same, start searching in ngram by word number <search_start_position>, and compare <num_collect> elements of vector <word_collect>, and <instace> descirbe which isntance it serach (nedded for seraching the same ngrams).  Function return pointer to  whole ngram converted to dynamic char array. If null returned it means thath ngram was not found.
@@ -78,37 +109,6 @@ char ** ngram_find_sufixs(ngram structure, char ** prefix, int num_prefix) {
 	return NULL;
 }
 
-/*add at end of stucture */
-int ngram_add(ngram * this, char ** words_collect, int num_words, char * file_name) {
-    /*check if there is space to add*/
-    if( (this->ngram_size) <= (this->ngram_elem) ) {
-	   /*resize double*/
-	   struct word_collect * new = (struct word_collect *) realloc( this->one_file, sizeof( * new) * this->ngram_size * 2);
-	   if( new == NULL )
-		  program_error(ERR_CRITIC, ERR_NGRAM_STRUCT, "Brak pamięci operacyjnej, by powiększyć strukturę przechowującą ngramy (ngram_add function error)");
-	   else 
-		  this->one_file= new;
-    }
-
-/*[IMPORTANT] New concept for this part, 
- * do not make new allocs, becasue it is not neccessery, just try to get to value
- * by using sqaure brackets exmp: this->one_file[this->ngram_elem]->words = words_collect
- */
-    /*making structure witch will be add*/
-    struct word_collect * newline = (struct word_collect *) malloc( sizeof(* newline) );
-    if( newline == NULL )
-		  program_error(ERR_CRITIC, ERR_NGRAM_STRUCT, "Brak pamięci operacyjnej, by stworzyc strukturę dla pojedyńczego zbioru ngramów (strukturę word_collect w ngram_add function)");
-
-    newline->words = words_collect;
-    newline->name_file = file_name;
-    newline->num_words = num_words;
-    
-    /*add created structure to end collection */ 
-    this->(one_file[this->ngram_elem]) = newline;
-    (this->ngram_elem)++;
-}
-
-char ** ngram_find_sufixs(ngram structure, char ** prefix, int num_prefix);
 
 /*free all memory which sturct ngram takes*/
 int ngram_free(ngram * this) {
